8a898a646e7f8a9c467f88f7c3040c0e9e576157
clab683 <daniel.claborne@pnnl.gov>
2021-05-05 11:13:48 -0700

    handle redis password/config

--- global.R
+++ global.R
@@ -16,7 +16,19 @@ for (f in Sys.glob("./ui_templates/*.R")) source(f, local = TRUE)
 # 
 miniocon = map_data_connection(config_file='minio_config.yml')
 
-url_prefix = if(Sys.getenv("SHINY_LOCAL_OR_NETWORK")=="local") "0.0.0.0" else "redis1"
-redis_url <- sprintf('redis://%s:6379', url_prefix)
+# Register url, this is running in another docker container alongside this one
+if(!file.exists("redis_config.yml")){
+  warning("No redis configuration found, attempting connection to default url: redis://redis1:6379")
+  redis_url <- "redis://redis1:6379"
+} else {
+  redis_cfg = yaml::read_yaml("redis_config.yml")
+  redis_host = if(Sys.getenv("SHINY_LOCAL_OR_NETWORK")=="local") "0.0.0.0" else redis_cfg[['host']]
+  redis_url <- sprintf('redis://:%s@%s:%s/%s', 
+                       redis_cfg[["password"]], 
+                       redis_host, 
+                       redis_cfg[['port']],
+                       redis_cfg[['db']])
+}
+message("Setting up redis connection at:  ", redis_url)
 
 sym_diff <- function(a,b) setdiff(union(a,b), intersect(a,b))

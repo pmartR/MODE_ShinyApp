7018f54d16f3f425f93caefa4266afb31543ad7d
David Degnan <40248272+David-Degnan@users.noreply.github.com>
2022-03-24 11:07:41 -0700

    Finally at trelliscope building step

--- observers/valid_parameters_warnings.R
+++ observers/valid_parameters_warnings.R
@@ -1,31 +1,15 @@
-#' @details Make sure that the column id and group specifications are valid to 
-#' create the trelliscope boxplots
-#' TODO:  When there are multiple plots, will require that we have selected the
-#' boxplots option to perform this check.
+#' @details Enable the make trelliscope button when the plot is ready 
 observe({
-  #' req(<we have selected boxplots as our panel>)
   
-  #' Get T/F of whether we pass certain conditions for making display, it should
-  #' always be T -> everything is good to go
-  cond_valid_groups <- tryCatch({
-    length(edata_groups()) == nrow(nested_edata()$data[[1]])
-  }, error = function(e) {
-    "__no_groups__"
-  })
+  # Accumulate different conditions
+  cond = TRUE
+  message = ""
   
-  disable_message = character(0)
-  
-  if(cond_valid_groups == "__no_groups__") {
-    disable_message <- c(disable_message, WARN_TEXT[["SPECIFY_GROUPS"]])
-    cond_valid_groups <- FALSE
-  } else if(!cond_valid_groups) {
-    disable_message <- c(disable_message, WARN_TEXT[["BAD_GROUP_LENGTH"]])
+  # If no trelliRow, let the user know
+  if (is.null(final_data$TrelliRow)) {
+    cond = FALSE
+    message = "This will be enabled once 'Confirm Plot' is clicked in Make Plot."
   }
-  
-  # accumulate different conditions
-  cond = cond_valid_groups
-  
-  disable_message = paste(disable_message, collapse = "\n")
   
   toggleState("make_trelliscope", condition = cond)
   
@@ -33,7 +17,7 @@ observe({
     session, 
     "make_trelliscope_disable_info", 
     condition = !cond,
-    tooltip_text = disable_message
+    tooltip_text = message
   )
   
 })

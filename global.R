e291ccba52ec731fbda0bec5d2c4c26915a24691
David Degnan <40248272+David-Degnan@users.noreply.github.com>
2022-07-18 14:03:50 -0700

    Working on redis example

--- global.R
+++ global.R
@@ -27,61 +27,8 @@ Minio_Test <- ifelse(Sys.getenv("MINIO_TEST") == "1", TRUE, FALSE)
 # Third, check if this is a local test version with redis
 Redis_Test <- ifelse(Sys.getenv("REDIS_TEST") == "1", TRUE, FALSE)
 
-# Check for minio if minio version or MAP is enabled 
-if (Minio_Test | MAP) {
-  
-  # Load map data access
-  library(mapDataAccess)
-  
-  # Connect to a local minio run 
-  source("./MAP_Functions.R", local = TRUE)
-  
-  # Create a reactive values to hold MAP specific objects
-  if (Minio_Test) {
-    MapConnect <- reactiveValues(MapConnect = map_data_connection(config_file = "./cfg/minio_config_local.yml"), Data = NULL)
-  } else if (MAP) {
-    MapConnect <- reactiveValues(MapConnect = map_data_connection(config_file = "minio_config.yml"), Data = NULL)
-  }
-    
-} else {
-  hide(id = "loading-gray-overlay")
-}
-
-# Add the redis container
-if (MAP | Redis_Test) {
-  
-  if (Redis_Test) {
-    redis_cfg = yaml::read_yaml("./cfg/redis_config.yml")
-    redis_host = "0.0.0.0" 
-    redis_url <- sprintf('redis://%s:%s/%s', 
-                         redis_host, 
-                         redis_cfg[['port']],
-                         redis_cfg[['db']])
-    
-    
-  } else if (MAP) {
-  
-    # Register url, this is running in another docker container alongside this one
-    if(!file.exists("redis_config.yml")){
-      warning("No redis configuration found, attempting connection to default url: redis://redis1:6379")
-      redis_url <- "redis://redis1:6379"
-    } else {
-      redis_cfg = yaml::read_yaml("redis_config.yml")
-      redis_host = redis_cfg[['host']]
-      redis_url <- sprintf('redis://%s:%s/%s', 
-                           redis_host, 
-                           redis_cfg[['port']],
-                           redis_cfg[['db']])
-    }
-  }
-  
-  message("Setting up redis connection at:  ", redis_url)
-  
-  # Import celery package from the virtual environment
-  clry <- reticulate::import('celery')
-  celery_app = clry$Celery('app', broker=redis_url, backend=redis_url)
-  
-}
+# Fourth, have an option for a docker compose local test
+Compose_Test <- ifelse(Sys.getenv("COMPOSE_TEST") == "1", TRUE, FALSE)
 
 # use this for all "None selected" options where applicable
 NOSELECT_ = "__nullselect__"
--- server.R
+++ server.R
@@ -1,6 +1,62 @@
 # Source all reactive ui elements, observers, reactive()
 # manually instantiate reactiveValues here 
 server <- function(input, output, session) {
+  
+  
+  # Check for minio if minio version or MAP is enabled 
+  if (Minio_Test | MAP) {
+    
+    # Load map data access
+    library(mapDataAccess)
+    
+    # Connect to a local minio run 
+    source("./MAP_Functions.R", local = TRUE)
+    
+    # Create a reactive values to hold MAP specific objects
+    if (Minio_Test) {
+      MapConnect <- reactiveValues(MapConnect = map_data_connection(config_file = "./cfg/minio_config_local.yml"), Data = NULL)
+    } else if (Compose_Test) {
+      MapConnect <- reactiveValues(MapConnect = map_data_connection(config_file = "./cfg/minio_config_compose.yml"), Data = NULL)
+    } else if (MAP) {
+      MapConnect <- reactiveValues(MapConnect = map_data_connection(config_file = "minio_config.yml"), Data = NULL)
+    }
+    
+  } else {
+    hide(id = "loading-gray-overlay")
+  }
+  
+  # Add the redis container
+  if (MAP | Redis_Test) {
+    
+    if (Redis_Test) {
+      
+      redis_url <- "redis://redis1:6379/0"
+      
+    } else if (MAP) {
+      
+      # Register url, this is running in another docker container alongside this one
+      if(!file.exists("./cfg/redis_config.yml")){
+        warning("No redis configuration found, attempting connection to default url: redis://redis1:6379")
+        redis_url <- "redis://redis1:6379/0"
+      } else {
+        redis_cfg = yaml::read_yaml("redis_config.yml")
+        redis_host = redis_cfg[['host']]
+        redis_url <- sprintf('redis://%s:%s/%s', 
+                             redis_host, 
+                             redis_cfg[['port']],
+                             redis_cfg[['db']])
+      }
+    }
+    
+    message("Setting up redis connection at:  ", redis_url)
+    
+    # Import celery package from the virtual environment
+    clry <- reticulate::import('celery')
+    celery_app = clry$Celery('app', broker=redis_url, backend=redis_url)
+    
+  }
+  
+  
   # source all 'modules'
   for(folder in c('reactive_variables', 'preprocessing', 'reactive_ui',
                   'plot_functions', 'observers')){
